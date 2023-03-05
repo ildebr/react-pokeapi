a301a02e8420b9f66d24681e1684dbc10697a06d
Ildebrando Quinchoa <ildebrandoefrain@gmail.com>
2023-03-05 12:16:11 -0400

    change next prev functionality

--- src/Pokeapi.js
+++ src/Pokeapi.js
@@ -1,15 +1,22 @@
 import './App.css';
+import React from "react";
 import useFetch from './useFetch';
 import Card from './Card';
 import { useState, useEffect } from "react"
+import {Link, useLocation} from "react-router-dom"
 import Loader from "./loader/Loader"
 function Pokeapi() {
   const url2 = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=8";
+  
   const urlSearch = "https://pokeapi.co/api/v2/pokemon/";
   
+  function useQuery() {
+    const { search } = useLocation();
   
-  
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  }
 
+  let query = useQuery()
   const[busqueUrl, setBusqueUrl] = useState(urlSearch);
   //fetches
   const {data: pores, isPending: pendientes, error:error3} = useFetch(busqueUrl)
@@ -29,11 +36,36 @@ function Pokeapi() {
   //const [pokeResults, setPokeResults] = useState({});
   //console.log("prueba " + dUrl)
   
+
+  const [page, setPage] = useState(0)
   useEffect(()=>{
     setUrlPage(url2);
   },[])
 
+  const [pokeurl, setPokeurl] = useState(`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=8`)
+  useEffect(() => {
+    if(!query.get('page')){
+      setPage(0)
+      setPokeurl(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=8`)
+    }else{
+      setPage(query.get('page'))
+      // n = query.get('page') * 8
+      setPokeurl(`https://pokeapi.co/api/v2/pokemon?offset=${query.get('page')*8}&limit=8`)
+    }
+    console.log(query.get('page'))
+  },[query.get('page')])
+
+  const {data: pokeresults, isPending: pokeispending, error:pokeerror} = useFetch(pokeurl)
+
+  useEffect(() => {
+    console.log(pokeurl)
+    
+    console.log(pokeresults)
+
+  },[pokeresults])
+
   // const {data: blogs, pendiente, error2} = useFetch(url2);
+  // where all pokemon are stored
   const {data: blogs, isPending:pendiente, error:error2} = useFetch(urlPage);
 
   
@@ -70,19 +102,23 @@ function Pokeapi() {
   const handleNext = (e) =>{
     //actualiza la url a la siguiente cantidad de resultados
     console.log("url siguiente " + blogs.next);
-    setUrlPage(blogs.next);
-
+    // setUrlPage(blogs.next);
+    // setPokeurl(page+1)
+    // window.history.replaceState(null, '', `pokemon?page=${Number(page)+1}`)
 
   }
 
   const handlePrev = (e) =>{
     //actualiza la url a la pagina anterior del url actual
     console.log("url anterior" + blogs.previous)
-    setUrlPage(blogs.previous)
+    // setUrlPage(blogs.previous)
+    setPokeurl(page-1)
   }
 
   return (
     <div className="Pokeapi">
+      <Link to={`?page=${Number(page)-1}`}  > Previous </Link>
+      <Link to={`?page=${Number(page)+1}`} > Next </Link>
       <header>
       <h2>Pokepi</h2>
 
@@ -91,7 +127,7 @@ function Pokeapi() {
         placeholder="Search Pokemon"
         value={searchTerm}
         onChange={handleOnChange} />
-        <button>Buscar</button>
+        <button>Search</button>
       </form>
       </header>
       
@@ -110,8 +146,8 @@ function Pokeapi() {
         
         
         { pendiente && <Loader />}
-            { error2 && <div>{error2}</div>}
-            { blogs && (
+            { pokeerror && <div>{error2}</div>}
+            { pokeresults && (
               <section className="fetch-section ">
                 <div className="buttons">
                   <button onClick={handlePrev}>Previous page</button>
@@ -119,8 +155,8 @@ function Pokeapi() {
                 </div>
                 <section className="pokemon-results">
                   
-                  {blogs.results.map((result) =>{
-                    return <Card pokemon={result.url} />
+                  {pokeresults.results.map((result) =>{
+                    return <Card pokemon={result.url} key={result.id} />
                   })}
                     
                 </section>
